c45d0b1c945cb284046c2aa26a494c7e7e4cbe95
Austinfranklin12 <55801294+Austinfranklin12@users.noreply.github.com>
2024-03-25 12:33:59 +0100

    new

--- web-rtc.js
+++ web-rtc.js
@@ -26,4 +26,79 @@ function handleError(error) {
 
 navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess).catch(handleError);
 
+// all the above works!
+
+
+
+
+
+
+
+
+
+
+
+// set up an asynchronous communication channel that will be
+// used during the peer connection setup
+const signalingChannel = new SignalingChannel(remoteClientId);
+signalingChannel.addEventListener('message', message => {
+    // New message from remote client received
+});
+
+// send an asynchronous message to the remote client
+signalingChannel.send('Hello!');
+
+// initiate peer connection
+async function makeCall() {
+    const configuration = {'iceServers': [{'urls': 'stun:stun.l.google.com:19302'}]}
+    const peerConnection = new RTCPeerConnection(configuration);
+    signalingChannel.addEventListener('message', async message => {
+        if (message.answer) {
+            const remoteDesc = new RTCSessionDescription(message.answer);
+            await peerConnection.setRemoteDescription(remoteDesc);
+        }
+    });
+    const offer = await peerConnection.createOffer();
+    await peerConnection.setLocalDescription(offer);
+    signalingChannel.send({'offer': offer});
+}
+
+// listen for local ICE candidates on the local RTCPeerConnection
+peerConnection.addEventListener('icecandidate', event => {
+    if (event.candidate) {
+        signalingChannel.send({'new-ice-candidate': event.candidate});
+    }
+});
+
+// listen for remote ICE candidates and add them to the local RTCPeerConnection
+signalingChannel.addEventListener('message', async message => {
+    if (message.iceCandidate) {
+        try {
+            await peerConnection.addIceCandidate(message.iceCandidate);
+        } catch (e) {
+            console.error('Error adding received ice candidate', e);
+        }
+    }
+});
+
+// listen for connectionstatechange on the local RTCPeerConnection
+peerConnection.addEventListener('connectionstatechange', event => {
+    if (peerConnection.connectionState === 'connected') {
+        // peers connected!
+    }
+});
+
+
+
+
+// on the receiving end...
+const peerConnection = new RTCPeerConnection(configuration);
+signalingChannel.addEventListener('message', async message => {
+    if (message.offer) {
+        peerConnection.setRemoteDescription(new RTCSessionDescription(message.offer));
+        const answer = await peerConnection.createAnswer();
+        await peerConnection.setLocalDescription(answer);
+        signalingChannel.send({'answer': answer});
+    }
+});
 
