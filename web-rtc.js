2a0ce0a662c16528424d75f1f319d778eb2762e1
Austinfranklin12 <55801294+Austinfranklin12@users.noreply.github.com>
2024-03-25 12:46:35 +0100

    update

--- web-rtc.js
+++ web-rtc.js
@@ -1,32 +1,32 @@
 
-// Put variables in global scope to make them available to the browser console.
-const audio = document.querySelector('audio');
+// // Put variables in global scope to make them available to the browser console.
+// const audio = document.querySelector('audio');
 
-const constraints = window.constraints = {
-  audio: true,
-  video: false
-};
+// const constraints = window.constraints = {
+//   audio: true,
+//   video: false
+// };
 
-function handleSuccess(stream) {
-  const audioTracks = stream.getAudioTracks();
-  console.log('Got stream with constraints:', constraints);
-  console.log('Using audio device: ' + audioTracks[0].label);
-  stream.oninactive = function() {
-    console.log('Stream ended');
-  };
-  window.stream = stream; // make variable available to browser console
-  audio.srcObject = stream;
-}
+// function handleSuccess(stream) {
+//   const audioTracks = stream.getAudioTracks();
+//   console.log('Got stream with constraints:', constraints);
+//   console.log('Using audio device: ' + audioTracks[0].label);
+//   stream.oninactive = function() {
+//     console.log('Stream ended');
+//   };
+//   window.stream = stream; // make variable available to browser console
+//   audio.srcObject = stream;
+// }
 
-function handleError(error) {
-  const errorMessage = 'navigator.MediaDevices.getUserMedia error: ' + error.message + ' ' + error.name;
-  document.getElementById('errorMsg').innerText = errorMessage;
-  console.log(errorMessage);
-}
+// function handleError(error) {
+//   const errorMessage = 'navigator.MediaDevices.getUserMedia error: ' + error.message + ' ' + error.name;
+//   document.getElementById('errorMsg').innerText = errorMessage;
+//   console.log(errorMessage);
+// }
 
-navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess).catch(handleError);
+// navigator.mediaDevices.getUserMedia(constraints).then(handleSuccess).catch(handleError);
 
-// all the above works!
+// // all the above works!
 
 
 
